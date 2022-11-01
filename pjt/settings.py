1a3fb0421de82867e6ed1ff5e718656068cecba3
wnsghd14 <wnsghd14@gmail.com>
2022-11-01 17:40:01 +0900

    base.html navbar및 검색창 구현

--- pjt/settings.py
+++ pjt/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
+import os
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
