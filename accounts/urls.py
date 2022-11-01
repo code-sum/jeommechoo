b469160107404a0c20f388ad92ecebe8b72e9cc1
code-sum <yisugyeong@gmail.com>
2022-11-01 11:21:02 +0900

    회원 프로필 페이지 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('<int:pk>/', views.detail, name='detail'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from django.contrib.auth import get_user_model
 
 def signup(request):
     if request.method == 'POST':
@@ -13,3 +14,10 @@ def signup(request):
         'form': form
     }
     return render(request, 'accounts/signup.html', context)
+
+def detail(request, pk):
+    user = get_user_model().objects.get(pk=pk)
+    context = {
+        'user': user
+    }
+    return render(request, 'accounts/detail.html', context)
