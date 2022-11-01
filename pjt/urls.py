5d7afa5b3f72833c46417976de39fe5b96e59884
code-sum <yisugyeong@gmail.com>
2022-11-01 16:33:02 +0900

    restaurants 앱 생성, URL 분리, index 페이지 작성

--- pjt/urls.py
+++ pjt/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main, name='main'), # 메인화면
     path('accounts/', include('accounts.urls')),
+    path('restaurants/', include('restaurants.urls')),
 ]
