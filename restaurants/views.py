33a29621c3aeb8c88ad9cae79d76cf5acfdc20a1
code-sum <yisugyeong@gmail.com>
2022-11-02 13:38:20 +0900

    레스토랑 디테일 페이지 구현

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_safe
+from .models import Restaurant
 
 def index(request):
     return render(request, 'restaurants/index.html')
+
+@require_safe
+def detail(request, pk):
+    restaurant = get_object_or_404(Restaurant, pk=pk)
+    context = {
+        'restaurant': restaurant
+    }
+    return render(request, 'restaurants/detail.html', context)
