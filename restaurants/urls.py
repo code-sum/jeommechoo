3fe56fbc71822eadab7040aa45cfca13d1dad034
code-sum <yisugyeong@gmail.com>
2022-11-02 22:28:00 +0900

    식당 정보 수정 기능 구현

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -5,6 +5,7 @@ app_name = 'restaurants'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:pk>/', views.detail, name='detail'),
     path('create/', views.create, name='create'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/update/', views.update, name='update'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_safe
 from .models import Restaurant
 from .forms import RestaurantForm
+from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     # 일단 가장 최근에 등록한 식당이 맨 앞에 오도록(추후 별점 반영해서 수정)
@@ -19,12 +21,13 @@ def detail(request, pk):
     }
     return render(request, 'restaurants/detail.html', context)
 
+@login_required
 def create(request):
     if not request.user.is_superuser:
-        return redirect('restaurants:index')
+        return HttpResponseForbidden()
     else:
         if request.method == 'POST':
-            form = RestaurantForm(request.POST)
+            form = RestaurantForm(request.POST, request.FILES)
             if form.is_valid():
                 form.save()
                 return redirect('restaurants:index')
@@ -34,3 +37,21 @@ def create(request):
             'form': form
         }
         return render(request, 'restaurants/new.html', context)
+
+@login_required
+def update(request, pk):
+    if not request.user.is_superuser:
+        return HttpResponseForbidden()
+    else:
+        restaurant = get_object_or_404(Restaurant, pk=pk)
+        if request.method == 'POST':
+            form = RestaurantForm(request.POST, request.FILES, instance=restaurant)
+            if form.is_valid():
+                form.save()
+                return redirect('restaurants:detail', restaurant.pk)
+        else:
+            form = RestaurantForm(instance=restaurant)
+        context = {
+            'form': form
+        }
+        return render(request, 'restaurants/update.html', context)
