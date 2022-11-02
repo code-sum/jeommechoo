dd4f5fd86cc1f868562b77180d838c423fd529ff
code-sum <yisugyeong@gmail.com>
2022-11-02 16:03:27 +0900

    회원관리 팔로우 기능 구현

--- accounts/models.py
+++ accounts/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
