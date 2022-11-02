27aeae98ac3c5c6625de7d532973ffb4fe6d260a
wnsghd14 <wnsghd14@gmail.com>
2022-11-02 17:56:26 +0900

    pjt urls에 static을 참조하지 않을시 발생되는 문제 해결

--- pjt/urls.py
+++ pjt/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("reviews/", include("reviews.urls")),
     path("restaurants/", include("restaurants.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
