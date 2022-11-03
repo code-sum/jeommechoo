87ba4025460c8821f7aefed5f038558e71210968
star2871 <star2871@gmail.com>
2022-11-03 13:32:16 +0900

    리뷰앱 디테일페이지 댓글 수정기능 빼고 완료, 리뷰작성 돌아가게 하기 완료, 리뷰 업데이트 기능 완료

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,15 +2,22 @@ from django import forms
 from .models import Review, Comment
 
 
+
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = [
+            'restaurant',
             "content",
             "image",
             "grade",
         ]
-
+        labels = {
+            'restaurant': '식당 이름', 
+            'content': '리뷰내용', 
+            'image': '메뉴 사진', 
+            'grade': '평점', 
+        }
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
